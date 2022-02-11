6069bf143634c48452391da39fa54ea7831cebb3
Joshua P Katz <joshpk105@gmail.com>
2022-02-11 14:05:39 -0500

    Added data documentation

---
+++ R/synth-data.R
@@ -0,0 +1,79 @@
+#' Synthetic RNA-Seq data run on the Ocean Genomics platform
+#'
+#' Synthetic RNA-seq data run against the Human Ensemble v101 database.
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://txomeai.oceangenomics.com/}{Ocean Genomics}
+#'
+#' @examples
+#' data(synthetic_data_report)
+#' head(synthetic_data_report$ls)
+"synthetic_data_report"
+
+#' Results from the Ocean Genomics Contamintaion Analysis
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://txomeai.oceangenomics.com/}{Ocean Genomics}
+#'
+#' @examples
+#' data(ContaminationSummary)
+#' head(ContaminationSummary)
+"ContaminationSummary"
+
+#' Results from the Ocean Genomics Differential Gene Expression analysis.
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://txomeai.oceangenomics.com/}{Ocean Genomics}
+#'
+#' @examples
+#' data(DifferentialGeneExpression)
+#' head(DifferentialGeneExpression)
+"DifferentialGeneExpression"
+
+#' Results from the Ocean Genomics High Variance Gene analysis.
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://txomeai.oceangenomics.com/}{Ocean Genomics}
+#'
+#' @examples
+#' data(HighVarianceGenes)
+#' head(HighVarianceGenes)
+"HighVarianceGenes"
+
+#' Results from the Ocean Genomics Spearman Correlation analysis.
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://txomeai.oceangenomics.com/}{Ocean Genomics}
+#'
+#' @examples
+#' data(SpearmanCorrelation)
+#' head(SpearmanCorrelation)
+"SpearmanCorrelation"
+
+#' Results from the Ocean Genomics Transcript Integrity analysis.
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://txomeai.oceangenomics.com/}{Ocean Genomics}
+#'
+#' @examples
+#' data(TranscriptIntegrityStats)
+#' head(TranscriptIntegrityStats)
+"TranscriptIntegrityStats"
